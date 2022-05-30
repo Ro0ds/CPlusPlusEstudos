5beb9fdfcf0f4806ccb71403bcd408b73f108838
Digo <digo_gabriel97@yahoo.com.br>
2022-05-30 15:56:35 -0300

    Address and Pointers

--- Principal.cpp
+++ Principal.cpp
@@ -4,26 +4,16 @@
 using namespace std;
 
 int main() {
-
-string food = "pizza";
-string &meal = food;
+string food = "Pizza";
+string* ptr = &food;
 
 cout << "Food: " << food << "\n";
-cout << "Meal: " << meal << "\n";
-cout << "Memory address of food: " << &food << "\n";
-cout << "Memory address of meal: " << &meal << "\n";
+cout << "Memory address of food: " <<  &food << "\n";
+cout << "Memory address with pointer: " << ptr << "\n";
 
-//References and Memory Address
-/*
-*   Cada variável tem seu espaço alocado na memória
-*   Caso eu crie a variável "food" ele pegará um espaço especifico na memoria
-*   Se eu criar outra chamada "meal" e fizer meal = food, ele criará um espaço novo na memoria para meal, porém pegando o valor atribuido a food
-*
-*   Caso você referencie a variavel food na variavel meal utilizando o &, "&meal = food"
-*   A variavel meal não irá alocar um novo espaço na memoria, irá utilizar o mesmo espaço que foi alocado para a variavel food, pq foi referenciada
-*   Deste modo, economizando memoria e aumentando a performance
-*
-*/
+// Variavel "food" armazena "Pizza"
+// "&food" referencia o endereço na memoria da variavel "food"
+// "ptr" armazena o endereço na memoria da variavel food
 
 return 0;
 }
