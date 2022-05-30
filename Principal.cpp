c39f22580272c2b8fec9cec961ec6c353769bafc
Digo <digo_gabriel97@yahoo.com.br>
2022-05-30 10:23:46 -0300

    References and Memory Address

--- Principal.cpp
+++ Principal.cpp
@@ -5,18 +5,25 @@ using namespace std;
 
 int main() {
 
-struct TipoDeVar {
-    int numero;
-    string nome;
-};
+string food = "pizza";
+string &meal = food;
 
-TipoDeVar teste;
+cout << "Food: " << food << "\n";
+cout << "Meal: " << meal << "\n";
+cout << "Memory address of food: " << &food << "\n";
+cout << "Memory address of meal: " << &meal << "\n";
 
-teste.nome = "Rodrigo";
-teste.numero = 1;
-
-cout << "Nome: " << teste.nome << "\n";
-cout << "Numero: " << teste.numero << "\n";
+//References and Memory Address
+/*
+*   Cada variável tem seu espaço alocado na memória
+*   Caso eu crie a variável "food" ele pegará um espaço especifico na memoria
+*   Se eu criar outra chamada "meal" e fizer meal = food, ele criará um espaço novo na memoria para meal, porém pegando o valor atribuido a food
+*
+*   Caso você referencie a variavel food na variavel meal utilizando o &, "&meal = food"
+*   A variavel meal não irá alocar um novo espaço na memoria, irá utilizar o mesmo espaço que foi alocado para a variavel food, pq foi referenciada
+*   Deste modo, economizando memoria e aumentando a performance
+*
+*/
 
 return 0;
 }
