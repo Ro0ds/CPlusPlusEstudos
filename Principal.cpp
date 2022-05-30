220ce06234aa6e5254f2eadeccc78047e8d43ffc
Digo <digo_gabriel97@yahoo.com.br>
2022-05-30 16:51:50 -0300

    Modifying pointers

--- Principal.cpp
+++ Principal.cpp
@@ -7,11 +7,24 @@ int main() {
 string food = "Pizza";
 string* ptr = &food;
 
-cout << "Memory address with the pointer: " << ptr << "\n";
-cout << "The value with the pointer: " << *ptr << "\n";
+cout << "Value of food: " << food << "\n";
+// food has "Pizza" as value
 
-// "&food" referencia o endereço na memoria da variavel "food"
-// "*ptr" retorna o valor bruto usando o endereço da memoria referenciada
+cout << "Memory address of food: " << &food << "\n";
+// the memory address is being referenced by the "&"
+
+cout << "Access the memory address of food and output its value: " << *ptr << "\n";
+// ptr is accessing the referenced memory address of food, *ptr will show the value using this memory address
+
+*ptr = "Hamburguer";
+// Assign a new value to the *ptr
+
+cout << "New pointer value: " << *ptr << "\n";
+// Printing the new pointer value for *ptr
+
+cout << "New food variable: " << food << "\n"; 
+// food variable changed as well
+
 
 return 0;
 }
