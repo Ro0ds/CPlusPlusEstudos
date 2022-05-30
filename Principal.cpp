7f55117d5b9fc28b38c4db11d0481a98cbcbb89f
Digo <digo_gabriel97@yahoo.com.br>
2022-05-30 17:01:52 -0300

    Functions - part 1

--- Principal.cpp
+++ Principal.cpp
@@ -2,29 +2,19 @@
 #include <string>
 
 using namespace std;
+int i = 0;
+
+void minhaFuncao();
 
 int main() {
-string food = "Pizza";
-string* ptr = &food;
+    for (int a = 1; a <= 10; a++){
+        minhaFuncao();
+    }
+    
+    return 0;
+}
 
-cout << "Value of food: " << food << "\n";
-// food has "Pizza" as value
-
-cout << "Memory address of food: " << &food << "\n";
-// the memory address is being referenced by the "&"
-
-cout << "Access the memory address of food and output its value: " << *ptr << "\n";
-// ptr is accessing the referenced memory address of food, *ptr will show the value using this memory address
-
-*ptr = "Hamburguer";
-// Assign a new value to the *ptr
-
-cout << "New pointer value: " << *ptr << "\n";
-// Printing the new pointer value for *ptr
-
-cout << "New food variable: " << food << "\n"; 
-// food variable changed as well
-
-
-return 0;
+void minhaFuncao(){
+    i++;
+    cout << "this function is being executed " << i << " times\n";
 }
